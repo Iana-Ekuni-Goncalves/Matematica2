645b14b42bb5ca566000380ab1f0664d075aeedf
Iana Ekuni Gonçalves <iana.goncalves@escola.pr.gov.br>
2025-05-27 08:03:20 -0300

    a

---
+++ assets/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Historia',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'Yuri Gagarin'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde se localiza Machu Picchu?',
+    'Peru'
+)
